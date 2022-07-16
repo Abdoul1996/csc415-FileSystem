f44508230f5dc37ff31b4775b3f9612169c984aa
Student <bbutler4@mail.sfsu.edu>
2022-07-16 16:35:00 -0700

    MS1 WIP

--- fsshell.c
+++ fsshell.c
@@ -1,9 +1,9 @@
 /**************************************************************
-* Class:  CSC-415-0# - Summer 2022
-* Names: 
-* Student IDs:
-* GitHub Name:
-* Group Name:
+* Class:  CSC-415-02 - Summer 2022
+* Names: Abdoulfatah Abdillahi, Brandon Butler, Jacob Cross, Kevin Manago
+* Student IDs: 921623492, 920819091, 916168167, 917784912
+* GitHub Name: Abdoul1996, BrandonButler08, JacobCross, Kmanago95
+* Group Name: Team Shield
 * Project: Basic File System
 *
 * File: fsShell.c
